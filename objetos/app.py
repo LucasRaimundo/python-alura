2c3d5ef48bc79c572cba2e4075f93809c18b5788
Lucas Raimundo <lucasraimundo2010@gmail.com>
2025-04-25 15:46:20 -0300

    applying polymorphism

--- objetos/app.py
+++ objetos/app.py
@@ -6,6 +6,8 @@ restaurante_praca = Restaurante('Brasileirinho', 'Marmita')
 
 nova_bebida = Bebida('Suco de limão', 5.0, 'Médio')
 prato = Prato('Aligot', 150.0, 'Pure de batatas com queijo')
+nova_bebida.aplicar_desconto()
+prato.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(nova_bebida)
 restaurante_praca.adicionar_no_cardapio(prato)
 
